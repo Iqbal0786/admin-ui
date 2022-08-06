638c4254344e25dbff6412d847d1709298675ca4
Mohammad Iqbal <iqbal240456@gmail.com>
2022-08-06 17:12:24 +0530

    added table and pagination

--- src/components/AdminUi.jsx
+++ src/components/AdminUi.jsx
@@ -1,17 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import TextField from "@mui/material/TextField"
 import Button from "@mui/material/Button"
+import ProductTable from './ProductTable';
+import axios from 'axios';
+import Pagination from "@mui/material/Pagination"
+import Stack from "@mui/material/Stack"
 
 export default function AdminUi() {
+    const [userData,setUserData]=useState([]);
+    const [paginatedData,setPaginatedData]=useState([]);
+    const [pageNumber,setPageNumber]=useState(1)
+    const pageSize= Math.ceil(userData.length/10)
+    let filterData= paginatedData || userData;
     function paginate(array, page_size, page_number) {
         // human-readable page numbers usually start with 1, so we reduce 1 in the first argument
         return array.slice((page_number - 1) * page_size, page_number * page_size);
       }
     //   let res= paginate([1,2,3,4,5,6,7],2,2);
     //   console.log(res)
+     
+    const paginationHandler=(e,value)=>{
+              //  default paginated data 
+              if(value>=1 && value<=pageSize){
+                let res=paginate(userData,10,value);
+                console.log(res)
+                 setPaginatedData([...res])
+              }
+              
+              
+         console.log(value);
+    }
+
+    useEffect(()=>{
+         axios.get("https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json").then((res)=>{console.log(res.data)
+          setUserData([...res.data])
+        }).catch((err)=>{
+            console.log(err)
+         })
+    },[])
+    
+    // calling paginate function to load default paginated data
+    useEffect(()=>{
+         let res=   paginate(userData,10,1);
+         console.log(res)
+          setPaginatedData([...res])
+    },[])
+    console.log(paginatedData)
   return (
    <>
    
@@ -23,14 +60,24 @@ export default function AdminUi() {
       </AppBar>
     </Box>
 
-    <Box sx={{width:"90%" , height:"800px" , margin:"auto" , marginTop:"100px"}}>
+    <Box sx={{width:"90%" , height:"800px" , margin:"auto" , marginTop:"50px"}}>
          {/* search box */}
         <Box sx={{width:"400px", margin:"auto" , display:"flex", justifyContent:"space-between"}}>
         <TextField variant='outlined' label="Search by name,email or role" sx={{width:"290px"}}/>
         <Button variant='outlined'>Search</Button>
         </Box>
 
-        
+        <ProductTable data={filterData}/>
+        <Box sx={{width:"90%" , margin:"auto", display:"flex",justifyContent:"space-between"}}>
+         <Box>
+            <Button variant='contained'>Delete Selected</Button>
+         </Box>
+         <Box>
+     <Stack spacing={2}>
+      <Pagination count={pageSize} color="primary" showFirstButton showLastButton onChange={paginationHandler}/>
+       </Stack>
+    </Box>
+        </Box>
         
     </Box>
    
---
+++ src/components/ProductTable.jsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import EditIcon from '@mui/icons-material/Edit';
+import Checkbox from '@mui/material/Checkbox';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+
+export default function ProductTable({data}) {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  return (
+    <Paper sx={{ width: '90%', overflow: 'hidden'  ,margin:"auto", marginTop:"40px", marginBottom:"50px"}}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow >
+              <TableCell > <Checkbox  disabled /></TableCell>
+              <TableCell sx={{fontWeight:"bolder"}}>Name</TableCell>
+              <TableCell sx={{fontWeight:"bolder"}}>Email</TableCell>
+              <TableCell sx={{fontWeight:"bolder"}}>Role</TableCell>
+              <TableCell sx={{fontWeight:"bolder"}}>Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((item)=>{
+              return (
+                <TableRow >
+              <TableCell > <Checkbox   /></TableCell>
+              <TableCell >{item.name}</TableCell>
+              <TableCell >{item.email}</TableCell>
+              <TableCell >{item.role}</TableCell>
+              <TableCell >
+                <EditIcon sx={{color:"green" , cursor:"pointer"}}/>     <DeleteIcon sx={{color:"red" , cursor:"pointer"}}/>
+              </TableCell>
+            </TableRow>
+              )
+            })}
+               
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
+  );
+}
